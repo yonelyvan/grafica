a0f909c3a501ebc305657c4f6e1dfede1d5c0d94
yonelyvan <yonelyvan@gmail.com>
2017-12-01 15:04:10 -0500

    points

--- project/test1.cpp
+++ project/test1.cpp
@@ -1,20 +1,29 @@
 //  g++ file.cpp -o m `pkg-config --cflags --libs opencv`
 #include "opencv2/opencv.hpp"
+#include <iostream>
 using namespace cv;
+using namespace std;
+
 int main(int argc, char** argv){
     VideoCapture cap;
     // open the default camera, use something different from 0 otherwise;
     // Check VideoCapture documentation.
     if(!cap.open(0))
         return 0;
-    for(;;)
-    {
+    while(1){
           Mat frame;
+          Mat contours;
           cap >> frame;
+          flip(frame, frame, 1);
           if( frame.empty() ) break; // end of video stream
-          imshow("this is you, smile! :)", frame);
-          if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC 
+          imshow("captura", frame);
+          Canny(frame, contours, 6000, 6000, 5, true);
+        imshow("MyNegativeVideo", contours);
+        if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC 
+    	//cout << "M = "<< endl << " "  << contours << endl << endl;
+    	//break;
     }
+
     // the camera will be closed automatically upon exit
     // cap.close();
     return 0;
